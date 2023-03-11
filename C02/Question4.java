8c9e9127b4df2f861fac72b8e7464bd3c53fb991
amjad718 <amjad.hussain718@gmail.com>
2023-03-11 12:22:38 +0530

    Small Changes

--- C02/Question4.java
+++ C02/Question4.java
@@ -5,38 +5,38 @@ class Employee{
     int eNo;
     int eSalary;
     String eName;
-    void addData(int eNo, String eName, int eSalary, int n){
+    Employee(int eNo, String eName, int eSalary){
         this.eNo = eNo;
         this.eName = eName;
         this.eSalary = eSalary;
     }
-//    void display(int number,String name,int salary, int n){
-//        for(int i=0; i<n; i++){
-//            System.out.println("Details of Employee "+i+1);
-//            System.out.println("Employee Number:"+number);
-//            System.out.println("Employee Name:"+name);
-//            System.out.println("Salary:"+salary);
-//        }
-//    }
 }
-
 
 public class Question4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of employee details to enter");
+        System.out.println("How much employee details do you want to enter?");
         int n = sc.nextInt();
         Employee[] emp = new Employee[n];
 
         for(int i=0; i<n; i++){
-            System.out.println("Enter the Employee "+i+" details (eNO, eName and eSalary)");
+            System.out.println("Enter the Employee "+(i+1)+" details (eNo, eName and eSalary)");
             int num = sc.nextInt();
             String name = sc.next();
             int salary = sc.nextInt();
-            emp[i+1].addData(num,name,salary,n);
+            emp[i] = new Employee(num,name,salary);
         }
+
+        System.out.println();
         System.out.println("Enter the eNo of the employee to find the details:");
         int No = sc.nextInt();
 
+        for(int i=0; i<n; i++) {
+            if (emp[i].eNo == No) {
+                System.out.println("Employee Number: "+emp[i].eNo);
+                System.out.println("Employee Name: "+emp[i].eName);
+                System.out.println("Employee Salary: "+emp[i].eSalary);
+            }
+        }
     }
 }
