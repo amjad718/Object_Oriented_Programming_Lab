ab96b6a13317088429ae88039fedaa32658cf7c4
amjad718 <amjad.hussain718@gmail.com>
2023-03-12 22:19:32 -0700

    small changes

--- C02/Question4.java
+++ C02/Question4.java
@@ -17,6 +17,7 @@ public class Question4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("How much employee details do you want to enter?");
         int n = sc.nextInt();
+      //Array of objects
         Employee[] emp = new Employee[n];
 
         for(int i=0; i<n; i++){
@@ -30,13 +31,19 @@ public class Question4 {
         System.out.println();
         System.out.println("Enter the eNo of the employee to find the details:");
         int No = sc.nextInt();
-
+        boolean flag = false;
+        //Searching
         for(int i=0; i<n; i++) {
             if (emp[i].eNo == No) {
+                flag = true;
                 System.out.println("Employee Number: "+emp[i].eNo);
                 System.out.println("Employee Name: "+emp[i].eName);
                 System.out.println("Employee Salary: "+emp[i].eSalary);
+                break;
             }
+        }
+        if(!flag){
+            System.out.println("The entered eNo is not available in the array..!!");
         }
     }
 }
